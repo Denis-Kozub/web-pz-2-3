da53b7afa8c1cf177dcba49fd31a53ccc9fc80d8
Denis_Kozub <kozubenkodennis@gmail.com>
2023-10-17 15:45:12 +0300

    fix

--- task_3/index.js
+++ task_3/index.js
@@ -1,6 +1,6 @@
 const table = document.querySelector(".table");
 
-let symbols = ["A", "B", "C", "D", "E", "F", "G",];
+let symbols = ["A", "B", "C", "D", "E", "F", "G","H"];
 
 for (let i = 0; i < 9; i++) {
     const row = document.createElement("div");
